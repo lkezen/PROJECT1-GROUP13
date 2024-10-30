12d2b2735c19934e6cdb0f68e05c80620fa1953c
lkezen <leticiakezen@Yahoo.com.br>
2024-10-30 12:29:18 -0400

    added mouse hover events

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,25 @@
 document.querySelector('.logo').textContent = "DEKORIOR";
 document.querySelector('.hero-text h1').style.color = "#DCAA37";
 
-// Example to add mouse events
+// Change navbar links color on hover
+document.querySelectorAll('.nav-links li a').forEach(function(link) {
+    link.addEventListener('mouseover', function() {
+        this.style.color = '#1B4A49';
+    });
+    link.addEventListener('mouseout', function() {
+        this.style.color = '#787878'; // Default color
+    });
+});
+
+// Change logo color on hover
+document.querySelector('.logo').addEventListener('mouseover', function() {
+    this.style.color = '#000000';
+});
+document.querySelector('.logo').addEventListener('mouseout', function() {
+    this.style.color = '#255751'; // Default color
+});
+
+// Change contact button background color on hover
 document.querySelector('.contact-button').addEventListener('mouseover', function() {
     this.style.backgroundColor = '#255751';
 });
