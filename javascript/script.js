fdd62934023776c87bd33ab0a232e8be41c3f397
lkezen <leticiakezen@Yahoo.com.br>
2024-10-30 13:18:57 -0400

    hover effect on logo text

--- javascript/script.js
+++ javascript/script.js
@@ -39,6 +39,9 @@ document.querySelectorAll('.contact-button, .submit-button').forEach(function(bu
     });
 });
 
+
+lalalala
+
 // Form submission with success message
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form from submitting the traditional way
